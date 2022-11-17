83acb64de32bfac3854e5029471e9ce084707155
Zainan Victor Zhou <zzn+github@zzn.im>
2022-11-17 14:14:07 -0800

    Refactor get ERC-5298 ready to be in contracts draft.

--- ERCs/eip-5298/contracts/IERC5298.sol
+++ ERCs/eip-5298/contracts/IERC5298.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-interface IERC5298 {
+interface IERC5298_CORE {
     function claimTo(
         address to,
         bytes32 ensNode,
         address operator,
         uint256 tokenId
     ) payable external;
+}
+
+interface IERC5298_GENERAL {
+    event AddedToHolding(bytes32 indexed ensNode, address indexed operator, uint256 indexed tokenId);
+    event RemovedFromHolding(bytes32 indexed ensNode, address indexed operator, uint256 indexed tokenId);
+    function addToHolding(bytes32 ensNode, address tokenContractAddress, uint256 tokenId) external payable;
+    function removeFromHolding(bytes32 ensNode, address tokenContractAddress, uint256 tokenId) external payable;
 }
