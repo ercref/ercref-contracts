44ddc5c9a809bcd14a5ee271b03adc0fb6853f99
xinbenlv <zzn-github@zzn.im>
2023-01-11 14:43:23 -0800

    Add EIP-5269 (#52)
    * Update
    
    * Add ERC5269 DRAFTv1
    
    * Update
    
    Co-authored-by: Zainan Victor Zhou <zzn+github@zzn.im>

---
+++ ERCs/eip-5269/README.md
@@ -0,0 +1,15 @@
+# ERC5269 RefImpl
+
+## Deployments
+
+1. Deployed and verified ERC5679Ext20RefImpl at goerli 0x672cf368Bf6c80517B43A83dD06A4041d7eD6C5E
+
+```sh
+yarn install
+npx hardhat test
+npx hardhat depver --network goerli --signer-index 0 --contract-name ERC721ForTesting
+```
+
+## Action Items
+
+- [ ] Write tests
---
+++ ERCs/eip-5269/contracts/ERC5269.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// Author: Zainan Victor Zhou <zzn-ercref@zzn.im>
+
+pragma solidity ^0.8.9;
+
+import "./IERC5269.sol";
+
+contract ERC5269 is IERC5269 {
+    bytes32 constant public EIP_STATUS = keccak256("DRAFTv1");
+    constructor () {
+        emit OnSupportEIP(address(0x0), 5269, bytes32(0), EIP_STATUS, "");
+    }
+
+    function _supportEIP(
+        address /*caller*/,
+        uint256 majorEIPIdentifier,
+        bytes32 minorEIPIdentifier,
+        bytes calldata /*extraData*/)
+    internal virtual view returns (bytes32 eipStatus) {
+        if (majorEIPIdentifier == 5269) {
+            if (minorEIPIdentifier == bytes32(0)) {
+                return EIP_STATUS;
+            }
+        }
+        return bytes32(0);
+    }
+
+    function supportEIP(
+        address caller,
+        uint256 majorEIPIdentifier,
+        bytes32 minorEIPIdentifier,
+        bytes calldata extraData)
+    external virtual view returns (bytes32 eipStatus) {
+        return _supportEIP(caller, majorEIPIdentifier, minorEIPIdentifier, extraData);
+    }
+}
