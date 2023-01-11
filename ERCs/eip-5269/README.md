c84ad1e51d5a4ed651109deae69f41293b2c0339
Zainan Victor Zhou <zzn+github@zzn.im>
2023-01-11 14:42:32 -0800

    Update

--- ERCs/eip-5269/README.md
+++ ERCs/eip-5269/README.md
@@ -1,12 +1,13 @@
-# ERC5679 RefImpl
+# ERC5269 RefImpl
 
 ## Deployments
 
 1. Deployed and verified ERC5679Ext20RefImpl at goerli 0x672cf368Bf6c80517B43A83dD06A4041d7eD6C5E
 
 ```sh
-npx hardhat run --network goerli scripts/deploy.ts
-npx hardhat verify --network goerli 0x672cf368Bf6c80517B43A83dD06A4041d7eD6C5E "ERC5679Ext20RefImpl v1.0.0" "ERC5679Ext20RefImpl"
+yarn install
+npx hardhat test
+npx hardhat depver --network goerli --signer-index 0 --contract-name ERC721ForTesting
 ```
 
 ## Action Items
--- ERCs/eip-5269/hardhat.config.ts
+++ ERCs/eip-5269/hardhat.config.ts
@@ -1,50 +1,12 @@
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
-import { HardhatUserConfig, task } from 'hardhat/config';
+import { HardhatUserConfig } from 'hardhat/config';
 import * as dotenv from 'dotenv';
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-depver";
+
 dotenv.config();
-
-task('deploy_n_verify', 'Deploy the contracts')
-  .setAction(async (args, hre) => {
-    async function deployByName(contractName: string, parameters: string[]): Promise<any> /*deployed address*/ {
-      console.log(`Deploying ${contractName} with parameters ${parameters}`);
-      const contractFactory = await hre.ethers.getContractFactory(contractName);
-      const contract = await contractFactory.deploy(...parameters);
-      await contract.deployed();
-      console.log(`${contractName} deployed to: ${contract.address}`);
-      let tx = contract.deployTransaction;
-      console.log(`Contract ${contractName} deployed to ${contract.address}`);
-      return { contract, tx };
-    }
-
-    const versionHex:string = "0x1002";
-
-    const network = hre.network.name;
-    console.log(`start deploy_n_verify version=${versionHex} on network ${network}`);
-    await hre.run('compile');
-
-    const results:any[] = [];
-    for (const contractName of ['ERC5679Ext20RefImpl', 'ERC5679Ext721RefImpl', 'ERC5679Ext1155RefImpl']) {
-      const { contract, tx } = await deployByName(contractName, [versionHex]);
-      results.push({ contract, tx });
-    }
-
-    for (const {contractName, contract, tx} of results) {
-      // console.log(`Waiting for tx ${tx.hash} to be mined`);
-      for (let i = 0; i < 10; i++) {
-        console.log(`Block ${i}...`);
-        await tx.wait(i);
-      }
-      console.log(`Done waiting for the confirmation for contract ${contractName} at ${contract.address}`);
-      await hre.run("verify:verify", {
-        address: contract.address,
-        constructorArguments: [versionHex],
-      }).catch(e=>console.log(`Failure ${e} when verifying ${contractName} at ${contract.address}`));
-    }
-    console.log(`done deploy_n_verify`);
-});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
